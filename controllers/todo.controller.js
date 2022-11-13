70bc9af372b3a54a5fbd5352e6c1be8c5afac56e
Aprilian Ihza <ihzaaprilian@gmail.com>
2022-11-13 19:47:45 +0700

    95%

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -29,7 +29,7 @@ module.exports = {
       })
     }
     } catch (error) {
-      console.log(error);
+      res.status(500).json({ message: "Server Error" })
     }
   },
 
@@ -56,7 +56,7 @@ module.exports = {
       res.json({message: "Data Deleted"})
     }
     } catch (error) {
-      console.log(error);
+      res.status(500).json({ message: "Server Error" })
     }
   },
 
@@ -71,7 +71,7 @@ module.exports = {
         data : todos })
    
     } catch (error) {
-      console.log(error);
+      res.status(500).json({ message: "Server Error" })
     }
   }
 
